ff6b2d7dc5106876e18b1cae8ea966af181b3233
Giustino Borzacchiello <giustinob@gmail.com>
2013-09-25 23:12:09 +0200

    Added routing to the server

--- NodeBeginner/server.js
+++ NodeBeginner/server.js
@@ -1,12 +1,15 @@
 var http = require("http");
 var url = require("url");
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
         // Retrieve the pathname. http://localhost:8888/pathname
         //                                              ^^^^^^^^
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
+
+        route(pathname);
+
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World");
         response.end();
